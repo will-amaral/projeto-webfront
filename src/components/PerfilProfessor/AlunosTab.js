e4e06d3c99cce387d27ba060e0fa5a4e186c7a47
Willian Amaral <willian.amaral@outlook.com.br>
2020-09-08 18:56:19 -0300

    Reset project

--- src/components/PerfilProfessor/AlunosTab.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function AlunosTab() {
-  return <h1>Lista de Alunos</h1>;
-}
--- src/utils/helpers.js
+++
@@ -1,6 +0,0 @@
-export function calculateAge(date) {
-  const ageDifference = Date.now() - date.getTime();
-  const ageDate = new Date(ageDifference);
-
-  return Math.abs(ageDate.getUTCFullYear() - 1970);
-}
